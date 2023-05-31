33ff42af8bcd99e39f642fe8752263a4c905445b
PILARGOD <psdanigodoy@gmail.com>
2023-05-31 19:56:42 -0300

    mi primera confirmación de actualización

--- mi-pagina-web.html/script.js
+++ mi-pagina-web.html/script.js
@@ -1,13 +1,33 @@
-function showalert () {alert("Hola,esto es una alerta desde javascript"); }
+function showAlert() {
+    alert('Hola, esta es una alerta de JavaScript');
+}
 
+const form = document.getElementById
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+function validateForm() {
+    const input = document.getElementById('input');
+    const inputValue = input.value;
+
+    if (!validateEmail(inputValue)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+if (form) {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        validateForm();
+    })
+}
 
 document.querySelector("button.button-menu-toggle")
     .addEventListener("click", function () {
-        document.querySelector(".nav-links").
-            classList.toggle("nav-links-responsive")
-    })
-
-
-
-
-
+        document.querySelector(".nav-links")
+    }
+    )
